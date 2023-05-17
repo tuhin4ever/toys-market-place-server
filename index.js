31633f61d481a49a963d5c3c22935193d9ddda04
tuhin_4ever <tuhin4ever19@gmail.com>
2023-05-17 20:53:08 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Toys Market place");
+});
+app.listen(port, () => {
+  console.log(`Toys Market place server is running on port: ${port}`);
+});
